285111d0baee042ee38a6e5a5a671ac6f73d6c77
JVsilveira <joaovitor.santossilveira@gmail.com>
2026-05-26 17:16:38 -0300

    feat: update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,20 @@
+import "./home.css"
+import Link from "next/link"
+
 export default function Home() {
   return (
     <div>
-      <main>Home</main>
+      <main>
+        <p className="welcome">Bem vindo a nossa central de bactérias</p>
+        <div className="home-container">
+          <Link href="/form">
+            <button className="button-home"> CADASTRAR</button>
+          </Link>
+          <Link href="/bacteria">
+            <button className="button-home"> BACTÉRIAS</button>
+          </Link>
+        </div>
+      </main>
     </div>
-  );
+  )
 }
