effe53586bc4d5ea169a02edf92e95b729eb186f
JVsilveira <joaovitor.santossilveira@gmail.com>
2026-05-26 17:51:17 -0300

    fix: using env.local

--- frontend/app/bacteria/page.tsx
+++ frontend/app/bacteria/page.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import Link from "next/link"
 import "./bacteria.css"
+
+import { useEffect, useState } from "react"
 
 interface Bacteria {
   id: number
@@ -8,22 +12,50 @@ interface Bacteria {
   gram: string
 }
 
-export default async function Bacteria() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/bacteria`)
-  const data: Bacteria[] = await response.json()
+export default function Bacteria() {
+  const [data, setData] = useState<Bacteria[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchBacteria() {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/bacteria`,
+        )
+
+        const result = await response.json()
+
+        setData(result)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchBacteria()
+  }, [])
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="bacteria-container">
       <h1 className="bac-title">Bactérias</h1>
+
       <ul className="bacteria-list">
         {data.map((bacterium: Bacteria) => (
           <li key={bacterium.id} className="bacteria-item">
             <h2>{bacterium.name}</h2>
+
             <p>{bacterium.description}</p>
+
             <p>Gram: {bacterium.gram}</p>
           </li>
         ))}
       </ul>
+
       <div className="container-button-bac">
         <Link href="/">
           <button className="button-home">VOLTAR</button>
