8a4fee71a36a242d22cf5e404c4532ae44b4aee0
JVsilveira <joaovitor.santossilveira@gmail.com>
2026-05-26 17:13:24 -0300

    feat: implements batecria

--- frontend/app/bacteria/page.tsx
+++ frontend/app/bacteria/page.tsx
@@ -1,30 +1,34 @@
+import Link from "next/link"
+import "./bacteria.css"
+
 interface Bacteria {
-  id: number;
-  name: string;
-  description: string;
-  gram: string;
+  id: number
+  name: string
+  description: string
+  gram: string
 }
 
 export default async function Bacteria() {
-  const response = await fetch("http://localhost:3001/bacteria");
-  const data: Bacteria[] = await response.json();
-  console.log(data);
+  const response = await fetch("http://localhost:3001/bacteria")
+  const data: Bacteria[] = await response.json()
 
   return (
-    <div>
-      <main>
-        <h1>Bactérias</h1>
-        <p>Lista de bactérias:</p>
-        <ul>
-          {data.map((bacterium: any) => (
-            <li key={bacterium.id}>
-              <h2>{bacterium.name}</h2>
-              <p>{bacterium.description}</p>
-              <p>Gram: {bacterium.gram}</p>
-            </li>
-          ))}
-        </ul>
-      </main>
+    <div className="bacteria-container">
+      <h1 className="bac-title">Bactérias</h1>
+      <ul className="bacteria-list">
+        {data.map((bacterium: Bacteria) => (
+          <li key={bacterium.id} className="bacteria-item">
+            <h2>{bacterium.name}</h2>
+            <p>{bacterium.description}</p>
+            <p>Gram: {bacterium.gram}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="container-button-bac">
+        <Link href="/">
+          <button className="button-home">VOLTAR</button>
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
