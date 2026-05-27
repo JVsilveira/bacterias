4877164733bf2b88e65c58717aa3c25208feb6c9
JVsilveira <joaovitor.santossilveira@gmail.com>
2026-05-26 22:47:41 -0300

    fix: center title

--- frontend/app/bacteria/page.tsx
+++ frontend/app/bacteria/page.tsx
@@ -17,11 +17,8 @@ interface PageProps {
 
 export default async function Bacteria({ searchParams }: PageProps) {
   const params = await searchParams;
-
   const search = params.search || "";
-
   let url = `${process.env.NEXT_PUBLIC_API_URL}/bacteria`;
-
   if (search.trim() !== "") {
     url = `${process.env.NEXT_PUBLIC_API_URL}/bacteria/search?name=${search}`;
   }
