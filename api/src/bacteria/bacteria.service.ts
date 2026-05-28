8055553d1eaa9ca9aac907b3281ffbc9387bf236
JVsilveira <joaovitor.santossilveira@gmail.com>
2026-05-27 21:54:50 -0300

    feat: add validations

--- api/src/bacteria/bacteria.service.ts
+++ api/src/bacteria/bacteria.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { Prisma } from '@prisma/client';
 
 import { PrismaService } from './database/prisma.service';
+
 import { BacteriaDto } from './dto/bacteria.dto';
 
 @Injectable()
@@ -23,18 +30,69 @@ export class BacteriaService {
   }
 
   async create(data: BacteriaDto) {
-    return await this.prisma.bacteria.create({ data });
+    try {
+      return await this.prisma.bacteria.create({
+        data,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException('Já existe uma bactéria com esse nome');
+      }
+
+      throw error;
+    }
   }
 
   async findOne(id: number) {
-    return await this.prisma.bacteria.findUnique({ where: { id } });
+    const bacteria = await this.prisma.bacteria.findUnique({
+      where: { id },
+    });
+
+    if (!bacteria) {
+      throw new NotFoundException('Bactéria não encontrada');
+    }
+
+    return bacteria;
   }
 
   async update(id: number, data: BacteriaDto) {
-    return await this.prisma.bacteria.update({ where: { id }, data });
+    try {
+      return await this.prisma.bacteria.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException('Já existe uma bactéria com esse nome');
+        }
+
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Bactéria não encontrada');
+        }
+      }
+
+      throw error;
+    }
   }
 
   async delete(id: number) {
-    return await this.prisma.bacteria.delete({ where: { id } });
+    try {
+      return await this.prisma.bacteria.delete({
+        where: { id },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException('Bactéria não encontrada');
+      }
+
+      throw error;
+    }
   }
 }
